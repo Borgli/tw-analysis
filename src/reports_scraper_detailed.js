d1f45d43d9c3060787c4692668f121923a3963f5
Hanna Borgli <Borgli@users.noreply.github.com>
2024-02-06 19:33:04 +0100

    Remove unused code in reports_scraper_detailed.js
    Deprecated code, including some regex constants and a commented-out section, were removed from 'reports_scraper_detailed.js'. This deletion streamlines the codebase, enhancing readability and maintenance. No functional changes were made in this commit.

--- src/reports_scraper_detailed.js
+++ src/reports_scraper_detailed.js
@@ -7,11 +7,7 @@ if (!document.URL.includes("screen=report&mode=defense")) {
 	throw new Error("Wrong page")
 }
 
-const re_speed = /-\s\((\w+)/
 const re_coord = /\d{1,3}\|\d{1,3}/g
-const re_distance = /distance ([\d.]+)/
-const re_sent = /sent\s+([A-Za-z]+\s+\d{2},\s+\d{2}:\d{2}:\d{2})/
-const re_arrival = /arrival\s+([A-Za-z]+\s+\d{2},\s+\d{2}:\d{2}:\d{2})/
 
 let data = "attacker,origin,target,arrival_time,speed,fake\n"
 
@@ -96,46 +92,6 @@ async function getInfoForEachReport() {
 	}
 }
 
-/*
-$("#report_list > tbody > tr").not(":first").not(":last").each(
-	(i, row) => {
-		if (i > 1) {
-			return
-		}
-		let description = $(row).find("td:eq(1) .quickedit-label").text()
-		let attacker = description.split('(')[0].trim()
-		let coords = description.match(re_coord)
-		let origin = coords[0]
-		let target = coords[1]
-		let is_fake = $(row).find("td:eq(1) img").attr("src").includes("attack_small")
-		let speed = 0;
-		let arrival_time;
-		$.get($(row).find("td:nth-child(2) > span.quickedit.report-title > span > a.report-link").attr("href"), (data) => {
-			const $report = $(data);
-			console.log("Get request called.")
-			let $arrival_time = $report.find("#content_value > table > tbody > tr > td:nth-child(2) > table > tbody > tr > td > table:nth-child(2) > tbody > tr:nth-child(2) > td:nth-child(2)")
-			arrival_time = $arrival_time.text().trim()
-			arrival_time = parseDate(arrival_time)
-			let attackers = []
-			$report.find("#attack_info_att_units > tbody > tr:nth-child(2) > td.unit-item").each(
-				(i, row) => {
-					attackers.push(parseInt($(row).text()))
-				}
-			)
-			// Check if fang
-			if (is_fake && attackers[9] > 50) {
-				is_fake = false;
-			}
-			speed = findSpeed(attackers);
-			//console.log($report.find("#content_value > table > tbody > tr > td:nth-child(2) > table > tbody > tr > td > table:nth-child(2) > tbody > tr:nth-child(1) > th:nth-child(2) > span > span > span").text())
-		}).then(() => {
-				data += `${attacker},${origin},${target},${arrival_time},${speed},${is_fake}\n`
-				console.log(`attacker: ${attacker}, origin: ${origin}, target: ${target}, arrival time: ${arrival_time}, speed: ${speed}, is fake: ${is_fake}`)
-			}
-		)
-	}
-)*/
-
 // https://stackoverflow.com/a/18197341
 function download(filename, text) {
 	var element = document.createElement('a');
@@ -151,6 +107,4 @@ function download(filename, text) {
 }
 
 getInfoForEachReport().then(r => download("data.csv", data));
-
-//download("data.csv", data)
 
